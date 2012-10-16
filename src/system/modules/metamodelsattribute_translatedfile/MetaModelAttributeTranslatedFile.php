28ac75a6445ee68e41abe452d86d17aa10bd2b64
Stefan heimes <stefan_heimes@hotmail.com>
2012-10-16 11:03:05 +0200

    Update multilanguage file tree.

--- src/system/modules/metamodelsattribute_translatedfile/MetaModelAttributeTranslatedFile.php
+++ src/system/modules/metamodelsattribute_translatedfile/MetaModelAttributeTranslatedFile.php
@@ -22,22 +22,25 @@
  * @subpackage  AttributeText
  * @author      Christian Schiffler <c.schiffler@cyberspectrum.de>
  */
-class MetaModelAttributeTranslatedFile
-extends MetaModelAttributeComplex
-implements IMetaModelAttributeTranslated
+class MetaModelAttributeTranslatedFile extends MetaModelAttributeTranslatedReference
 {
-        protected $arrMeta = array();
+
+	protected $arrMeta = array();
 	protected $arrAux = array();
 	protected $arrProcessed = array();
 	protected $auxDate = array();
 	protected $multiSRC = array();
-        
-        
-        /////////////////////////////////////////////////////////
+
+	protected function getValueTable()
+	{
+		return 'tl_metamodel_translatedlongblob';
+	}
+
+	/////////////////////////////////////////////////////////
 	// Helper
 	/////////////////////////////////////////////////////////
-        
-        /**
+
+	/**
 	 * Parse the meta.txt file of a folder. This is an altered version and differs from the
 	 * Contao core funtion as it also checks the fallback language.
 	 *
@@ -117,7 +120,7 @@ implements IMetaModelAttributeTranslated
 
 		$this->parseMetaFile(dirname($strFile), true);
 
-		$arrMeta =$this->arrMeta[dirname($strFile)][$objFile->basename];
+		$arrMeta = $this->arrMeta[dirname($strFile)][$objFile->basename];
 
 		$strBasename = strlen($arrMeta[0]) ? $arrMeta[0] : specialchars($objFile->basename);
 		$strAltText = (strlen($arrMeta[0]) ? $arrMeta[0] : ucfirst(str_replace('_', ' ', preg_replace('/^[0-9]+_/', '', $objFile->filename))));
@@ -126,18 +129,18 @@ implements IMetaModelAttributeTranslated
 
 		$strIcon = 'system/themes/' . MetaModelController::getTheme() . '/images/' . $objFile->icon;
 		$arrSource = array
-		(
-			'file'	=> $strFile,
-			'mtime'	=> $objFile->mtime,
-			'alt'	=> $strAltText,
-			'caption' => (strlen($arrMeta[2]) ? $arrMeta[2] : ''),
-			'title' => $strBasename,
-			'metafile' => $arrMeta,
-			'icon' => $strIcon,
-			'size' => $objFile->filesize,
-			'sizetext' => sprintf('(%s)', MetaModelController::getReadableSize($objFile->filesize, 2)),
-			'url' => Environment::getInstance()->request . (($GLOBALS['TL_CONFIG']['disableAlias'] || !$GLOBALS['TL_CONFIG']['rewriteURL']
-&& count($_GET) || strlen($_GET['page'])) ? '&amp;' : '?'). 'file=' . MetaModelController::urlEncode($strFile)
+		    (
+		    'file' => $strFile,
+		    'mtime' => $objFile->mtime,
+		    'alt' => $strAltText,
+		    'caption' => (strlen($arrMeta[2]) ? $arrMeta[2] : ''),
+		    'title' => $strBasename,
+		    'metafile' => $arrMeta,
+		    'icon' => $strIcon,
+		    'size' => $objFile->filesize,
+		    'sizetext' => sprintf('(%s)', MetaModelController::getReadableSize($objFile->filesize, 2)),
+		    'url' => Environment::getInstance()->request . (($GLOBALS['TL_CONFIG']['disableAlias'] || !$GLOBALS['TL_CONFIG']['rewriteURL']
+		    && count($_GET) || strlen($_GET['page'])) ? '&amp;' : '?') . 'file=' . MetaModelController::urlEncode($strFile)
 		);
 
 		// images
@@ -150,13 +153,15 @@ implements IMetaModelAttributeTranslated
 			if ($showImage)
 			{
 				$strSrc = MetaModelController::getImage(MetaModelController::urlEncode($strFile), $intWidth, $intHeight, $strMode);
-			} else {
+			}
+			else
+			{
 				$strSrc = $strFile;
 			}
 			$arrSource['src'] = $strSrc;
 
 			$size = getimagesize(TL_ROOT . '/' . urldecode($strSrc));
-			$arrSource['lb'] = 'lb'.$strId;
+			$arrSource['lb'] = 'lb' . $strId;
 			$arrSource['w'] = $size[0];
 			$arrSource['h'] = $size[1];
 			$arrSource['wh'] = $size[3];
@@ -164,133 +169,8 @@ implements IMetaModelAttributeTranslated
 
 		return $arrSource;
 	}
-        
-        /////////////////////////////////////////////////////////
-	// interface IMetaModelAttribute
-	/////////////////////////////////////////////////////////
-
-	public function getAttributeSettingNames()
-	{
-		return array_merge(parent::getAttributeSettingNames(), array(
-			'file_multiple',
-			'file_customFiletree',
-			'file_uploadFolder',
-			'file_validFileTypes',
-			'file_filesOnly',
-		));
-	}
-
-	public function getFieldDefinition()
-	{
-		$arrFieldDef=parent::getFieldDefinition();
-		$arrFieldDef['inputType'] = 'fileTree';
-
-		$arrFieldDef['eval']['files'] = true;
-
-		$arrFieldDef['eval']['fieldType'] = $this->get('file_multiple') ? 'checkbox' : 'radio';
-
-		$arrFieldDef['eval']['extensions'] = $GLOBALS['TL_CONFIG']['allowedDownload'];
-
-		if ($this->get('file_customFiletree'))
-		{
-			if (strlen($this->get('file_uploadFolder')))
-			{
-				$arrFieldDef['eval']['path'] = $this->get('file_uploadFolder');
-			}
-			if (strlen($this->get('file_validFileTypes')))
-			{
-				$arrFieldDef['eval']['extensions'] = $this->get('file_validFileTypes');
-			}
-			if (strlen($this->get('file_filesOnly')))
-			{
-				$arrFieldDef['eval']['filesOnly'] = true;
-			}
-		}
-		return $arrFieldDef;
-	}
-
-	public function valueToWidget($varValue)
-	{
-		return $varValue['value'];
-	}
-
-	public function getDataFor($arrIds)
-	{
-		$strActiveLanguage = $this->getMetaModel()->getActiveLanguage();
-		$strFallbackLanguage = $this->getMetaModel()->getFallbackLanguage();
-
-		$arrReturn = $this->getTranslatedDataFor($arrIds, $strActiveLanguage);
-
-		// second round, fetch fallback languages if not all items could be resolved.
-		if ((count($arrReturn) < count($arrIds)) && ($strActiveLanguage != $strFallbackLanguage))
-		{
-			$arrFallbackIds = array();
-			foreach ($arrIds as $intId)
-			{
-				if (empty($arrReturn[$intId]))
-				{
-					$arrFallbackIds[] = $intId;
-				}
-			}
-
-			if ($arrFallbackIds)
-			{
-				$arrFallbackData = $this->getTranslatedDataFor($arrFallbackIds, $strFallbackLanguage);
-				// cannot use array_merge here as it would renumber the keys.
-				foreach ($arrFallbackData as $intId => $arrValue)
-				{
-					$arrReturn[$intId] = $arrValue;
-				}
-			}
-		}
-		return $arrReturn;
-	}
-        
-        
-
-	public function setDataFor($arrValues)
-	{
-		foreach ($this->getMetaModel()->getAvailableLanguages() as $strLangCode)
-		{
-			$this->setTranslatedDataFor($arrValues, $strLangCode);
-		}
-	}
-
-	public function unsetDataFor($arrIds)
-	{
-		foreach ($this->getMetaModel()->getAvailableLanguages() as $strLangCode)
-		{
-			$this->unsetValueFor($arrIds, $strLangCode);
-		}
-	}
 
 	/**
-	 * {@inheritdoc}
-	 *
-	 * Fetch filter options from foreign table.
-	 *
-	 */
-	public function getFilterOptions($arrIds = array())
-	{
-		$objDB = Database::getInstance();
-
-		if ($arrIds)
-		{
-			$strWhereIds = ' AND item_id IN (' . implode(',', $arrIds) . ')';
-		}
-
-		$objValue = $objDB->prepare('SELECT * FROM tl_metamodel_translatedlongtext WHERE att_id=? AND langcode=? ' . $strWhereIds)
-				->execute($this->get('id'), $this->getMetaModel()->getActiveLanguage());
-
-		$arrReturn = array();
-		while ($objValue->next())
-		{
-			$arrReturn[$objValue->value] = $objValue->value;
-		}
-		return $arrReturn;
-	}
-        
-        /**
 	 * when rendered via a template, this returns the values to be stored in the template.
 	 */
 	protected function prepareTemplate(MetaModelTemplate $objTemplate, $arrRowData, $objSettings = null)
@@ -303,7 +183,7 @@ implements IMetaModelAttributeTranslated
 
 		$arrFiles = array();
 
-		foreach ((array)$arrRowData[$this->getColName()] as $strFile)
+		foreach ((array) $arrRowData[$this->getColName()] as $strFile)
 		{
 			if (is_file(TL_ROOT . '/' . $strFile))
 			{
@@ -363,7 +243,7 @@ implements IMetaModelAttributeTranslated
 			case 'random':
 				$keys = array_keys($arrFiles);
 				shuffle($keys);
-				foreach($keys as $key)
+				foreach ($keys as $key)
 				{
 					$files[$key] = $arrFiles[$key];
 				}
@@ -373,7 +253,7 @@ implements IMetaModelAttributeTranslated
 		if ($objSettings->file_sortBy != 'meta')
 		{
 			// re-sort the values
-			foreach($arrFiles as $k=>$v)
+			foreach ($arrFiles as $k => $v)
 			{
 				$files[] = $arrFiles[$k];
 				$source[] = $arrSource[$k];
@@ -382,15 +262,15 @@ implements IMetaModelAttributeTranslated
 
 		// add the classes now the values have been sorted.
 		$countFiles = count($source);
-		foreach($source as $k=>$v)
+		foreach ($source as $k => $v)
 		{
 			$source[$k]['class'] = (($k == 0) ? ' first' : '')
-				. (($k == ($countFiles -1 )) ? ' last' : '')
+				. (($k == ($countFiles - 1 )) ? ' last' : '')
 				. ((($k % 2) == 0) ? ' even' : ' odd');
 		}
 
-		$objTemplate->files	= $files;
-		$objTemplate->src 	= $source;
+		$objTemplate->files = $files;
+		$objTemplate->src = $source;
 
 		$this->arrMeta = array();
 		$this->arrAux = array();
@@ -399,63 +279,109 @@ implements IMetaModelAttributeTranslated
 		$this->multiSRC = array();
 	}
 
+	/////////////////////////////////////////////////////////
+	// interface IMetaModelAttribute
+	/////////////////////////////////////////////////////////
+
+	public function getAttributeSettingNames()
+	{
+		return array_merge(parent::getAttributeSettingNames(), array(
+			    'file_multiple',
+			    'file_customFiletree',
+			    'file_uploadFolder',
+			    'file_validFileTypes',
+			    'file_filesOnly',
+			));
+	}
+
+	public function getFieldDefinition()
+	{
+		$arrFieldDef = parent::getFieldDefinition();
+
+		$arrFieldDef['inputType'] = 'fileTree';
+
+		$arrFieldDef['eval']['files'] = true;
+
+		$arrFieldDef['eval']['fieldType'] = $this->get('file_multiple') ? 'checkbox' : 'radio';
+
+		$arrFieldDef['eval']['extensions'] = $GLOBALS['TL_CONFIG']['allowedDownload'];
+
+		if ($this->get('file_customFiletree'))
+		{
+			if (strlen($this->get('file_uploadFolder')))
+			{
+				$arrFieldDef['eval']['path'] = $this->get('file_uploadFolder');
+			}
+			if (strlen($this->get('file_validFileTypes')))
+			{
+				$arrFieldDef['eval']['extensions'] = $this->get('file_validFileTypes');
+			}
+			if (strlen($this->get('file_filesOnly')))
+			{
+				$arrFieldDef['eval']['filesOnly'] = true;
+			}
+		}
+
+		return $arrFieldDef;
+	}
+
+	public function valueToWidget($varValue)
+	{
+		return deserialize($varValue['value']);
+	}
+
+	public function widgetToValue($varValue, $intId)
+	{
+		return array
+		    (
+		    'tstamp' => time(),
+		    'value' => $varValue,
+		    'att_id' => $this->get('id'),
+//			'langcode' => $strLangCode,
+//			'item_id' => $intId,
+		);
+	}
+
 	/////////////////////////////////////////////////////////////////
 	// interface IMetaModelAttributeTranslated
 	/////////////////////////////////////////////////////////////////
 
-	public function setTranslatedDataFor($arrValues, $strLangCode)
+	protected function getSetValues($arrValue, $intId, $strLangCode)
 	{
-		$objDB = Database::getInstance();
-		// first off determine those to be updated and those to be inserted.
-		$arrIds = array_keys($arrValues);
-		$arrExisting = array_keys($this->getTranslatedDataFor($arrIds, $strLangCode));
-		$arrNewIds = array_diff($arrIds, $arrExisting);
+		if (is_array($arrValue['value']) && count($arrValue['value']) != 0)
+		{
+			$arrReturn = array(
+			    'tstamp' => time(),
+			    'value' => serialize($arrValue['value']),
+			    'att_id' => $this->get('id'),
+			    'langcode' => $strLangCode,
+			    'item_id' => $intId,
+			);
+		}
+		else if (!is_array($arrValue['value']) && strlen($arrValue['value']) != 0)
+		{
+			$arrReturn = array(
+			    'tstamp' => time(),
+			    'value' => $arrValue['value'],
+			    'att_id' => $this->get('id'),
+			    'langcode' => $strLangCode,
+			    'item_id' => $intId,
+			);
+		}
+		else
+		{
+			$arrReturn = array(
+			    'tstamp' => time(),
+			    'value' => null,
+			    'att_id' => $this->get('id'),
+			    'langcode' => $strLangCode,
+			    'item_id' => $intId,
+			);
+		}
 
-		// now update...
-		foreach ($arrExisting as $intId)
-		{
-			$objDB->prepare('UPDATE tl_metamodel_translatedlongblob SET value=?, tstamp=? WHERE att_id=? AND langcode=? AND item_id=?')
-				  ->execute($arrValues[$intId], time(), $this->get('id'), $strLangCode, $intId);
-		}
-		// ...and insert
-		foreach ($arrNewIds as $intId)
-		{
-			$objDB->prepare('INSERT INTO tl_metamodel_translatedlongblob %s')
-				  ->set(array(
-				  'tstamp' => time(),
-				  'value' => $arrValues[$intId],
-				  'att_id' => $this->get('id'),
-				  'langcode' => $strLangCode,
-				  'item_id' => $intId,
-				  ))
-				  ->execute();
-		}
-	}
-
-	/**
-	 * Get values for the given items in a certain language.
-	 */
-	public function getTranslatedDataFor($arrIds, $strLangCode)
-	{
-		$objDB = Database::getInstance();
-		$objValue = $objDB->prepare('SELECT * FROM tl_metamodel_translatedlongblob WHERE att_id=? AND langcode=? AND item_id IN (' . implode(',', $arrIds) . ')')
-				->execute($this->get('id'), $strLangCode);
-		$arrReturn = array();
-		while ($objValue->next())
-		{
-			$arrReturn[$objValue->item_id] = $objValue->row();
-		}
 		return $arrReturn;
 	}
 
-	/**
-	 * Remove values for items in a certain lanugage.
-	 */
-	public function unsetValueFor($arrIds, $strLangCode)
-	{
-		$objValue = $objDB->prepare('DELETE FROM tl_metamodel_translatedlongblob WHERE att_id=? AND langcode=? AND item_id IN (' . implode(',', $arrIds) . ')')
-				->execute($this->get('id'), $strLangCode);
-	}
 }
 
 ?>
