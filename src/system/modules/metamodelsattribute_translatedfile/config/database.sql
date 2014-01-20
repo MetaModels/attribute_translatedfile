1ce47b8f96ec8b62e766a478de8e6b82725ebfdc
Andreas Isaak <andy.jared@googlemail.com>
2014-01-20 20:46:04 +0100

    Delete unused comma

--- src/system/modules/metamodelsattribute_translatedfile/config/database.sql
+++ src/system/modules/metamodelsattribute_translatedfile/config/database.sql
@@ -20,7 +20,7 @@ CREATE TABLE `tl_metamodel_translatedlongblob` (
   `value` longblob NULL,
   PRIMARY KEY  (`id`),
   KEY `attlang` (`att_id`, `langcode`),
-  KEY `attitem` (`att_id`, `item_id`),
+  KEY `attitem` (`att_id`, `item_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
@@ -33,7 +33,7 @@ CREATE TABLE `tl_metamodel_attribute` (
   `file_customFiletree` char(1) NOT NULL default '',
   `file_uploadFolder` blob NULL,
   `file_validFileTypes` varchar(255) NOT NULL default '',
-  `file_filesOnly` char(1) NOT NULL default '',
+  `file_filesOnly` char(1) NOT NULL default ''
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
@@ -44,5 +44,5 @@ CREATE TABLE `tl_metamodel_rendersetting` (
   `file_sortBy` varchar(32) NOT NULL default '',
   `file_showLink` char(1) NOT NULL default '',
   `file_showImage` char(1) NOT NULL default '',
-  `file_imageSize` varchar(255) NOT NULL default '',
+  `file_imageSize` varchar(255) NOT NULL default ''
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
