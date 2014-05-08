98c585239875e0dd073da82e78827b7f82340a6a
Mini Model <minimodel@metamodel.me>
2014-05-08 08:14:43 +0200

    Updated translations from transifex.

--- src/system/modules/metamodelsattribute_translatedfile/languages/rm/tl_metamodel_attribute.php
+++ src/system/modules/metamodelsattribute_translatedfile/languages/rm/tl_metamodel_attribute.php
@@ -8,8 +8,17 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-13T22:13:26+02:00
+ * last-updated: 2013-07-10T10:05:51+02:00
  */
 
-
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_customFiletree']['0']      = 'Persunalisar l\'ierarchia da datotecas';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_filesOnly']['0']           = 'Be lubir datotecas';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_filesOnly']['1']           = 'Tscherna questa opziun per restrenscher il navigatur da datotecas sin datotecas (ordinaturs na pon betg vegnir tschernids).';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_multiple']['0']            = 'Selecziun multipla';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_multiple']['1']            = 'Sche questa chaschetta vegn tschernida po l\'utilisader tscherner dapli che in element.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_uploadFolder']['0']        = 'Definir l\'ordinatur da basa';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_uploadFolder']['1']        = 'Tscherna il punct da basa dal qual l\'utilisader vegn a tscherner quest champ da datotecas.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_validFileTypes']['0']      = 'Tips da datotecas valids';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_validFileTypes']['1']      = 'Endatescha ina glista seperada entras commas dad extensiuns da datotecas validas per quest champ.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['translatedfile'] = 'Datoteca translatada';
+
