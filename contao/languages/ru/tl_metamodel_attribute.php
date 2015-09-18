4303c40fdf1d8a2664549d6a7c0ceb64ca9b8383
Mini Model <minimodel@metamodel.me>
2015-09-18 10:31:24 +0200

    Updated translations from transifex.

---
+++ contao/languages/ru/tl_metamodel_attribute.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/ru/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-12-14T16:13:25+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_customFiletree']['0']      = 'Настроить дерево файлов';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_customFiletree']['1']      = 'Позволяет задать пользовательские параметры для дерева файлов.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_filePicker']['0']          = 'Использовать выбор файлов';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_filePicker']['1']          = 'Если выбрано, использовать выбор файлов вместо дерева файлов.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_filesOnly']['0']           = 'Разрешить только файлы';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_filesOnly']['1']           = 'Выберите опцию для ограничения файлового браузера выбором только файлов (каталоги не выбираются).';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_multiple']['0']            = 'Множественный выбор';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_multiple']['1']            = 'Если выбрано, пользователь сможет выбирать более одного элемента.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_uploadFolder']['0']        = 'Установить корневой каталог';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_uploadFolder']['1']        = 'Выбирает корневую точку, из которой пользователь сможет выбрать поле файла.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_validFileTypes']['0']      = 'Допустимые типы файлов';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_validFileTypes']['1']      = 'Введите разделенный запятыми список расширений допустимых типов файлов для данного поля.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['translatedfile'] = 'Переводить файл';
+
