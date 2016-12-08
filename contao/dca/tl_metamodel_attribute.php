37f72d838fd5ff44164d5acfbeb513b64be8c6f0
Sven Baumann <baumann.sv@gmail.com>
2016-12-08 12:51:24 +0100

    for feature deep delete add data provider and child condition configuration

--- contao/dca/tl_metamodel_attribute.php
+++ contao/dca/tl_metamodel_attribute.php
@@ -7,6 +7,7 @@
  * data in each collection.
  *
  * PHP version 5
+ *
  * @package    MetaModels
  * @subpackage AttributeTranslatedFile
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
@@ -22,42 +23,85 @@
  * Table tl_metamodel_attribute
  */
 
+/**
+ * Add palette configuration.
+ */
 $GLOBALS['TL_DCA']['tl_metamodel_attribute']['metapalettes']['translatedfile extends _complexattribute_'] = array(
     '+advanced' => array('file_customFiletree', 'file_multiple'),
     '+display'  => array('-width50'),
 );
 
 $GLOBALS['TL_DCA']['tl_metamodel_attribute']['metasubpalettes']['file_customFiletree'] = array(
-    'file_uploadFolder', 'file_validFileTypes', 'file_filesOnly',
+    'file_uploadFolder',
+    'file_validFileTypes',
+    'file_filesOnly',
 );
 
+/**
+ * Add data provider.
+ */
+$GLOBALS['TL_DCA']['tl_metamodel_attribute']['dca_config']['data_provider']['tl_metamodel_translatedlongblob'] =
+    array
+    (
+        'source' => 'tl_metamodel_translatedlongblob'
+    );
+
+/**
+ * Add child condition.
+ */
+$GLOBALS['TL_DCA']['tl_metamodel_attribute']['dca_config']['childCondition'][] = array
+(
+    'from'   => 'tl_metamodel_attribute',
+    'to'     => 'tl_metamodel_translatedlongblob',
+    'setOn'  => array
+    (
+        array
+        (
+            'to_field'   => 'att_id',
+            'from_field' => 'id',
+        ),
+    ),
+    'filter' => array
+    (
+        array
+        (
+            'local'     => 'att_id',
+            'remote'    => 'id',
+            'operation' => '=',
+        ),
+    )
+);
+
+/**
+ * Add field configuration.
+ */
 $GLOBALS['TL_DCA']['tl_metamodel_attribute']['fields']['file_customFiletree'] = array(
-    'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_customFiletree'],
-    'inputType'               => 'checkbox',
-    'eval'                    => array('submitOnChange' => true, 'tl_class' => 'w50'),
+    'label'     => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_customFiletree'],
+    'inputType' => 'checkbox',
+    'eval'      => array('submitOnChange' => true, 'tl_class' => 'w50'),
 );
 
 $GLOBALS['TL_DCA']['tl_metamodel_attribute']['fields']['file_multiple'] = array(
-    'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_multiple'],
-    'inputType'               => 'checkbox',
-    'eval'                    => array('submitOnChange' => true, 'tl_class' => 'w50'),
+    'label'     => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_multiple'],
+    'inputType' => 'checkbox',
+    'eval'      => array('submitOnChange' => true, 'tl_class' => 'w50'),
 );
 
 $GLOBALS['TL_DCA']['tl_metamodel_attribute']['fields']['file_uploadFolder'] = array(
-    'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_uploadFolder'],
-    'exclude'                 => true,
-    'inputType'               => 'fileTree',
-    'eval'                    => array('fieldType' => 'radio', 'tl_class' => 'clr'),
+    'label'     => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_uploadFolder'],
+    'exclude'   => true,
+    'inputType' => 'fileTree',
+    'eval'      => array('fieldType' => 'radio', 'tl_class' => 'clr'),
 );
 
 $GLOBALS['TL_DCA']['tl_metamodel_attribute']['fields']['file_validFileTypes'] = array(
-    'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_validFileTypes'],
-    'inputType'               => 'text',
-    'eval'                    => array('maxlength' => 255, 'tl_class' => 'w50'),
+    'label'     => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_validFileTypes'],
+    'inputType' => 'text',
+    'eval'      => array('maxlength' => 255, 'tl_class' => 'w50'),
 );
 
 $GLOBALS['TL_DCA']['tl_metamodel_attribute']['fields']['file_filesOnly'] = array(
-    'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_filesOnly'],
-    'inputType'               => 'checkbox',
-    'eval'                    => array('tl_class' => 'w50 m12'),
+    'label'     => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_filesOnly'],
+    'inputType' => 'checkbox',
+    'eval'      => array('tl_class' => 'w50 m12'),
 );
