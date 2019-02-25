086997f96fe76701361a770742dba6a8581694ae
Ingolf Steinhardt <info@e-spin.de>
2019-02-25 16:10:27 +0100

    Hofix clear DCA rendersettings - we use the same settings as attribute file require attribute_file

--- src/Resources/contao/dca/tl_metamodel_rendersetting.php
+++ src/Resources/contao/dca/tl_metamodel_rendersetting.php
@@ -17,6 +17,7 @@
  * @author     David Greminger <david.greminger@1up.io>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     David Molineus <david.molineus@netzmacht.de>
+ * @author     Ingolf Steinhardt <info@e-spin.de>
  * @copyright  2012-2019 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_translatedfile/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
@@ -24,54 +25,4 @@
 
 $GLOBALS['TL_DCA']['tl_metamodel_rendersetting']['metapalettes']['translatedfile extends default'] = array(
     '+advanced' => array('file_sortBy', 'file_showLink', 'file_showImage'),
-);
-
-$GLOBALS['TL_DCA']['tl_metamodel_rendersetting']['metasubpalettes']['file_showImage'] = array(
-    'file_imageSize',
-);
-
-$GLOBALS['TL_DCA']['tl_metamodel_rendersetting']['fields']['file_sortBy'] = array(
-    'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['file_sortBy'],
-    'exclude'                 => true,
-    'inputType'               => 'select',
-    'options'                 => array('name_asc', 'name_desc', 'date_asc', 'date_desc', 'meta', 'random'),
-    'reference'               => &$GLOBALS['TL_LANG']['tl_metamodel_rendersetting'],
-    'eval'                    => array(
-        'tl_class'            => 'w50',
-        'chosen'              => true,
-    ),
-    'sql'                     => 'varchar(32) NOT NULL default \'\''
-);
-
-$GLOBALS['TL_DCA']['tl_metamodel_rendersetting']['fields']['file_showLink'] = array(
-    'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['file_showLink'],
-    'inputType'               => 'checkbox',
-    'eval'                    => array('tl_class' => 'w50 m12'),
-    'sql'                     => 'char(1) NOT NULL default \'\''
-);
-
-$GLOBALS['TL_DCA']['tl_metamodel_rendersetting']['fields']['file_showImage'] = array(
-    'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['file_showImage'],
-    'inputType'               => 'checkbox',
-    'eval'                    => array(
-        'submitOnChange'      => true,
-        'tl_class'            => 'clr',
-    ),
-    'sql'                     => 'char(1) NOT NULL default \'\''
-);
-
-$GLOBALS['TL_DCA']['tl_metamodel_rendersetting']['fields']['file_imageSize'] = array(
-    'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['file_imageSize'],
-    'exclude'                 => true,
-    'inputType'               => 'imageSize',
-    'options'                 => $GLOBALS['TL_CROP'],
-    'reference'               => &$GLOBALS['TL_LANG']['MSC'],
-    'eval'                    => array(
-        'rgxp'                => 'digit',
-        'includeBlankOption'  => true,
-        'nospace'             => true,
-        'helpwizard'          => true,
-        'tl_class'            => 'w50',
-    ),
-    'sql'                     => 'varchar(255) NOT NULL default \'\''
 );
