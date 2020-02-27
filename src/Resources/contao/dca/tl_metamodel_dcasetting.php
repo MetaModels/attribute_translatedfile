58a19dd11ab7d25af0abbaec9d8b581507bb0d87
Ingolf Steinhardt <info@e-spin.de>
2020-02-27 15:40:03 +0100

    ad manual sorting

--- src/Resources/contao/dca/tl_metamodel_dcasetting.php
+++ src/Resources/contao/dca/tl_metamodel_dcasetting.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_translatedfile.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2020 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -17,7 +17,8 @@
  * @author     David Greminger <david.greminger@1up.io>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2012-2019 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2020 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_translatedfile/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  * @filesource
@@ -28,6 +29,7 @@ $GLOBALS['TL_DCA']['tl_metamodel_dcasetting']['metasubselectpalettes']['attr_id'
     'tl_class'
     ],
     'functions'    => [
-        'mandatory'
+        'mandatory',
+        'file_widgetMode'
     ]
 ];
