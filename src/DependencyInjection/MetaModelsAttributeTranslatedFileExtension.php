bf134b4d09e0439254170ddaf308ccb6191fd8e5
Ingolf Steinhardt <info@e-spin.de>
2022-12-28 23:53:21 +0100

    Add schema management

--- src/DependencyInjection/MetaModelsAttributeTranslatedFileExtension.php
+++ src/DependencyInjection/MetaModelsAttributeTranslatedFileExtension.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_translatedfile.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2022 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -13,7 +13,8 @@
  * @package    MetaModels/attribute_translatedfile
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2012-2019 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2022 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_translatedfile/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -41,5 +42,9 @@ class MetaModelsAttributeTranslatedFileExtension extends Extension
         $loader->load('factory.yml');
         $loader->load('event_listeners.yml');
         $loader->load('services.yml');
+
+        $managedSchemaTypeNames   = $container->getParameter('metamodels.managed-schema-type-names') ?? [];
+        $managedSchemaTypeNames[] = 'translatedfile';
+        $container->setParameter('metamodels.managed-schema-type-names', $managedSchemaTypeNames);
     }
 }
