e79aeb0dfae9743c268767163bb7145773249b30
zonky <info@e-spin.de>
2023-07-17 20:36:43 +0200

    Add feature to chose own widget template

--- src/Resources/contao/dca/tl_metamodel_dcasetting.php
+++ src/Resources/contao/dca/tl_metamodel_dcasetting.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_translatedfile.
  *
- * (c) 2012-2020 The MetaModels team.
+ * (c) 2012-2023 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -18,7 +18,7 @@
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @author     Ingolf Steinhardt <info@e-spin.de>
- * @copyright  2012-2020 The MetaModels team.
+ * @copyright  2012-2023 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_translatedfile/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  * @filesource
@@ -26,7 +26,8 @@
 
 $GLOBALS['TL_DCA']['tl_metamodel_dcasetting']['metasubselectpalettes']['attr_id']['translatedfile'] = [
     'presentation' => [
-    'tl_class'
+        'tl_class',
+        'be_template',
     ],
     'functions'    => [
         'mandatory',
