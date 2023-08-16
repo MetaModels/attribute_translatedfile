fe90ad729fcf02a1fa33000cd1925709a2b10073
Mini Model <minimodel@metamodel.me>
2023-08-16 21:03:27 +0200

    Updated translations from transifex.

--- src/Resources/contao/languages/it/tl_metamodel_attribute.php
+++ src/Resources/contao/languages/it/tl_metamodel_attribute.php
@@ -1,14 +1,24 @@
 <?php
+
 /**
- * Translations are managed using Transifex. To create a new translation
+ * This file is part of MetaModels/attribute_translatedfile.
+ *
+ * (c) 2012-2023 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * Translations are managed automatically using Transifex. To create a new translation
  * or to help to maintain an existing one, please register at transifex.com.
  *
- * @link http://help.transifex.com/intro/translating.html
- * @link https://www.transifex.com/projects/p/metamodels/language/it/
+ * Last-updated: 2023-08-16T21:03:27+02:00
  *
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
- *
- * last-updated: 2017-08-24T07:59:27+02:00
+ * @copyright 2012-2023 The MetaModels team.
+ * @license   https://github.com/MetaModels/attribute_translatedfile/blob/master/LICENSE LGPL-3.0-or-later
+ * @link      https://www.transifex.com/metamodels/public/
+ * @link      https://www.transifex.com/signup/?join_project=metamodels
  */
 
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_customFiletree']['0']      = 'Personalizza la struttura ad albero dei file';
@@ -22,4 +32,3 @@ $GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_uploadFolder']['1']        =
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_validFileTypes']['0']      = 'Tipologia file validi';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_validFileTypes']['1']      = 'Inserire i tipi di file che si vuole visualizzare. Utilizzare il simbolo pipe come separatore.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['translatedfile'] = 'File tradotto';
-
